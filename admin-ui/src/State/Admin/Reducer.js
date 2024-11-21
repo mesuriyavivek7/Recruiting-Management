318ce3e84b174c597e54bb8e8147124b10d8387c
mesuriyavivek7 <vivekmesuriya6@gmail.com>
2024-11-21 14:47:50 +0530

    cookies-change-11

--- admin-ui/src/State/Admin/Reducer.js
+++ admin-ui/src/State/Admin/Reducer.js
@@ -23,28 +23,16 @@ if(!localUserData){
 const initialState = {
   userData: localUserData
 };
+
 const AdminReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_USER_DATA':
-
-      const adminCookie = Cookies.get('admin_user');
-      console.log(adminCookie)
-      if (adminCookie) {
         localStorage.setItem('userData', JSON.stringify(action.payload));
         return {
           ...state,
           userData: action.payload,
         };
 
-      }
-      else {
-
-        localStorage.removeItem('userData');
-        return {
-          ...state,
-          userData: null,
-        };
-      }
     default:
       return state;
   }
