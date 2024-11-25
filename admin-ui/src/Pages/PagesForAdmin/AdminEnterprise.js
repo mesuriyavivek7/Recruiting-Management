d46d05c4849b8dc31086163ff0d656a8e862b019
mesuriyavivek7 <vivekmesuriya6@gmail.com>
2024-11-25 19:20:19 +0530

    active/inactive account done

--- admin-ui/src/Pages/PagesForAdmin/AdminEnterprise.js
+++ admin-ui/src/Pages/PagesForAdmin/AdminEnterprise.js
@@ -1,25 +1,101 @@
-import React, { useState } from 'react'
-import { Button } from '@mui/material';
+import React, { useEffect, useState } from 'react'
 import EnterpriseDetails from './EnterpriseDetails';
 import EnterpriseTeam from './EnterpriseTeam';
 import EnterpriseJob from './EnterpriseJob';
 import { useLocation } from 'react-router-dom';
+import Notification from '../../Components/Notification';
+import { useSelector } from 'react-redux';
+import {
+  Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions,
+  TextField
+} from '@mui/material';
+import { fetchEnterpriseById } from '../../services/api';
+import axios from 'axios';
 
 const AdminEnterprise = () => {
+  const myValue = useSelector((state) => state.admin);
   const location = useLocation();
-  const { enterpriseDetails } = location.state || {}; // Access the passed state
+  const [inactivateLoad, setInactivateLoad] = useState(false);
+  const [enterpriseDetails,setEnterpriseDetails] = useState(location.state.enterpriseDetails || {}); // Access the passed state
+  
   const [activeTab, setActiveTab] = useState('Enterprise Details');
+  const [accountStatus,setAccountStatus]=useState('Active')
+  const [openpopup, setOpenpopup] = useState(false);
+  const [reason, setReason] = useState('');
+
+  const [notification, setNotification] = useState(null);
+
+  const showNotification = (message, type) => {
+    setNotification({ message, type });
+  };
+
+  useEffect(()=>{
+     if(enterpriseDetails){
+        setAccountStatus(enterpriseDetails?.account_status.status)
+     }
+  },[enterpriseDetails])
+
+  const handleCloseInactivateButton = () => {
+    setOpenpopup(false);
+  };
+
+  const handleInactivateButton = async (e) => {
+    e.stopPropagation();
+    if (accountStatus === 'Active') {
+      setOpenpopup(true);
+    } else {
+      try {
+        await axios.post(`${process.env.REACT_APP_API_APP_URL}/enterprise/changestatus`, {
+          id: enterpriseDetails._id,
+          status: enterpriseDetails.account_status.status,
+          reason,
+          admin_id: myValue.userData._id,
+        });
+        showNotification('Successfully changed account status.', 'success');
+        const updateEnterprise=await fetchEnterpriseById(enterpriseDetails._id)
+        setEnterpriseDetails(updateEnterprise)
+      } catch (err) {
+        showNotification('Error changing account status!', 'failure');
+      }
+    }
+  };
+
+  const handleSubmitButton = async () => {
+    try {
+      setInactivateLoad(true);
+      await axios.post(`${process.env.REACT_APP_API_APP_URL}/enterprise/changestatus`, {
+        id: enterpriseDetails._id,
+        status: enterpriseDetails.account_status.status,
+        reason,
+        admin_id: myValue.userData._id,
+      });
+      setOpenpopup(false);
+      const updateEnterprise=await fetchEnterpriseById(enterpriseDetails._id)
+       setEnterpriseDetails(updateEnterprise)
+      showNotification('Successfully changed account status.', 'success');
+    } catch (err) {
+      showNotification('Error changing account status!', 'failure');
+    }
+    setInactivateLoad(false);
+  };
+
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
   };
   return (
     <div>
-
+     {notification && (
+        <Notification
+          open={true}
+          message={notification.message}
+          type={notification.type}
+          onClose={() => setNotification(null)}
+        />
+      )}
       <div className='pt-6'>
-
+      <div className='flex justify-between items-center'>
         <div className="flex gap-0">
-
           <Button
             id="demo-customized-button"
             aria-haspopup="true"
@@ -79,7 +155,37 @@ const AdminEnterprise = () => {
           >
             Jobs
           </Button>
+          </div>
 
+          <button onClick={handleInactivateButton} className={`${accountStatus==="Active"?("bg-orange-400 hover:bg-orange-500"):"bg-green-400 hover:bg-green-500"} tracking-wide rounded-sm text-[20px] p-2 w-32`}>
+            {accountStatus==="Active"?"Inactivate":"Activate"}
+          </button>
+
+          {/* Dialog for inactivating an enterprise */}
+      <Dialog open={openpopup} onClose={handleCloseInactivateButton}>
+        <DialogTitle>Reason for Inactivating</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Provide a reason for inactivating this enterprise:
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="reason"
+            label="Reason"
+            type="text"
+            fullWidth
+            value={reason}
+            onChange={(e) => setReason(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseInactivateButton} color="primary">Cancel</Button>
+          <Button onClick={handleSubmitButton} color="primary" disabled={inactivateLoad || !reason}>
+            {inactivateLoad ? 'Inactivating...' : 'Submit'}
+          </Button>
+        </DialogActions>
+      </Dialog>
         </div>
         <div>
           {activeTab === 'Enterprise Details' && (
